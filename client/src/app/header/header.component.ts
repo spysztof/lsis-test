044b15b914bb4345238577e38c565666c29e2482
Krzysztof Spychała <spysztof@gmail.com>
2019-09-02 14:00:50 +0200

    create header coponent

---
+++ client/src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lsis-client-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
